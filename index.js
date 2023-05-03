db8f422d396c757a319e377ed4e1292bc3be69a5
Jordan Porter <s.jordanporter@hotmail.com>
2023-05-02 20:03:23 -0600

    final

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceTraveled){
+    if (distanceTraveled <= 400) {
+      return 'This one is on me!';
+    } else if(distanceTraveled <= 2000) {
+      return 'That will be twenty bucks.'
+    } else if(distanceTraveled <= 2500) {
+      return 'I will gladly take your thirty bucks.'
+    } else if(distanceTraveled >= 2501) {
+     return 'No can do.'
+    }
+  }
+
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  switch(tipAmount){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
